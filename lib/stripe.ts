4330466988a174bc8a228d248bf58dc99cae15b6
richardelles1 <richardelles@gmail.com>
2025-05-28 23:28:39 -0700

    🔧 Fix Stripe API version typing

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,9 @@
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15' as unknown as Stripe.LatestApiVersion,
+ apiVersion: '2022-11-15' as Stripe.LatestApiVersion,
+
+
   typescript: true,
 })
 
